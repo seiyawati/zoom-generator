eadaee085608764f25a47fa267008694c589a3c6
Seiya Kawamoto <kid960805@gmail.com>
2021-02-27 14:32:39 +0900

    add: LP Template

---
+++ components/Footer.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import TwitterIcon from "@material-ui/icons/Twitter";
+
+const useStyles = makeStyles({
+  root: {},
+});
+
+export default function Footer() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.root}
+      style={{ color: "#e0f2f1", backgroundColor: "#0E5D5D" }}
+    >
+      <BottomNavigationAction
+        label="Twitter"
+        icon={<TwitterIcon />}
+        style={{ color: "white" }}
+      ></BottomNavigationAction>
+      <BottomNavigationAction
+        label="GitHub"
+        icon={<GitHubIcon />}
+        style={{ color: "white" }}
+      />
+    </BottomNavigation>
+  );
+}
---
+++ components/Header.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import HelpIcon from "@material-ui/icons/Help";
+import HomeIcon from "@material-ui/icons/Home";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar
+        position="static"
+        style={{ color: "#e0f2f1", backgroundColor: "#0E5D5D" }}
+      >
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Site Name
+          </Typography>
+          <Button>
+            <HomeIcon style={{ color: "white" }} />
+          </Button>
+          <Button>
+            <HelpIcon style={{ color: "white" }} />
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
