4c61cf89651bb798dd84093a7f6af80d160fb0bd
emap-strawberry <melon.pan.san@gmail.com>
2021-02-27 22:32:40 +0900

    add sample background pattern

--- components/generator/Card/WallPaper.js
+++ components/generator/Card/WallPaper.js
@@ -15,28 +15,28 @@ import {
   getSkillLevel,
 } from "components/generator/store";
 
-// const pattern =  {
-//   pattern1: {
-//     backgroundColor: "",
-//     color: ,
-//     fontFamily:
-//   },
-//   pattern2: {
-//     backgroundColor: ,
-//     color: ,
-//     fontFamily:
-//   },
-//   pattern3: {
-//     backgroundColor: ,
-//     color: ,
-//     fontFamily:
-//   },
-//   pattern4: {
-//     backgroundColor: ,
-//     color: ,
-//     fontFamily:
-//   },
-// };
+const pattern =  {
+ pattern1: {
+   backgroundColor: "#0097A7",
+   color: "white",
+   fontFamily: "sans-serif",
+ },
+ pattern2: {
+   backgroundColor: "#E57E7E",
+   color: "white",
+   fontFamily: "sans-serif",
+ },
+ pattern3: {
+   backgroundColor: "#F57C00",
+   color: "white",
+   fontFamily: "sans-serif",
+ },
+ pattern4: {
+   backgroundColor: "#388E3C",
+   color: "white",
+   fontFamily: "sans-serif",
+ },
+};
 
 const styles = {
   main: {
