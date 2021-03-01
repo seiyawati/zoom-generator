9326d78a5a7be95016c5887e381edd70e654a518
Seiya Kawamoto <kid960805@gmail.com>
2021-03-01 19:55:30 +0900

    fix: Head

--- pages/generator/index.js
+++ pages/generator/index.js
@@ -1,34 +1,39 @@
-import React from 'react'
+import React from "react";
 // recoil
-import { RecoilRoot } from 'recoil'
+import { RecoilRoot } from "recoil";
 // material ui core components
 import Header from "components/Header";
 import Footer from "components/Footer";
 import Copyright from "components/Copyright";
-import Profile from 'components/generator/Profile/Profile'
-import Skill from 'components/generator/Skill/Skill'
-import Social from 'components/generator/Socials/Social'
-import Background from 'components/generator/Background/Background'
-import WallPaper from 'components/generator/Card/WallPaper'
-import GridItem from 'components/generator/TimComponents/components/Grid/GridItem'
-import GridContainer from 'components/generator/TimComponents/components/Grid/GridContainer'
-import { makeStyles } from '@material-ui/core/styles'
-import { CssBaseline } from '@material-ui/core'
+import Profile from "components/generator/Profile/Profile";
+import Skill from "components/generator/Skill/Skill";
+import Social from "components/generator/Socials/Social";
+import Background from "components/generator/Background/Background";
+import WallPaper from "components/generator/Card/WallPaper";
+import GridItem from "components/generator/TimComponents/components/Grid/GridItem";
+import GridContainer from "components/generator/TimComponents/components/Grid/GridContainer";
+import { makeStyles } from "@material-ui/core/styles";
+import { CssBaseline } from "@material-ui/core";
+import Head from "next/head";
 
 const style = {
   main: {
     overflow: "hidden",
   },
   body: {
-    padding: '1rem'
-  }
-}
+    padding: "1rem",
+  },
+};
 
-const useStyles = makeStyles(style)
+const useStyles = makeStyles(style);
 function Generator() {
-  const classes = useStyles()
+  const classes = useStyles();
   return (
     <div className={classes.main}>
+      <Head>
+        <title>Generator</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header />
       <div className={classes.body}>
         <GridContainer>
@@ -47,15 +52,15 @@ function Generator() {
       <Footer />
       <Copyright />
     </div>
-  )
+  );
 }
 
 function App() {
-    return (
-        <RecoilRoot>
-            <Generator />
-        </RecoilRoot>
-    )
+  return (
+    <RecoilRoot>
+      <Generator />
+    </RecoilRoot>
+  );
 }
 
-export default App
+export default App;
